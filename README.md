bac78b23b252f5ba4e7d555aff9a6239e0275017
RaquelMichelon <raquelmichelonaulas@gmail.com>
2023-07-28 08:15:50 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# PowerCATS-homePage
+
+CulturalCare is a responsive Webpage to show cultural events to users. This project is in the development phase.
+
+Click [here](https://...) to see the project on-line.
+
+## Installation
+
+- Clone the project;
+- Open it in your browser;
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ js/main.js
@@ -0,0 +1,75 @@
+window.addEventListener("scroll", onScroll);
+
+onScroll()
+
+function onScroll() {
+    showNavOnScroll()
+    backToTopButtonOnScroll()
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
+
+}
+
+function activateMenuAtCurrentSection(section) {
+    const targetLine = scrollY + innerHeight / 2 
+
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+    const sectionEndsAt = sectionTop + sectionHeight
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+    const sectionBoudaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+    const sectionId = section.getAttribute('id')
+
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+
+    if(sectionBoudaries) {
+        menuElement.classList.add('active')
+    }
+}
+
+function showNavOnScroll() {
+    if(scrollY > 0) {
+        navigation.classList.add("scroll")
+    } else {
+        navigation.classList.remove("scroll")
+    }
+}
+
+function backToTopButtonOnScroll() {
+    if(scrollY > 400) {
+        backToTopButton.classList.add("show")
+    } else {
+        backToTopButton.classList.remove("show")
+    }
+}
+
+function openMenu() {
+    document.body.classList.add("menu-expanded")
+}
+
+function closeMenu() {
+    document.body.classList.remove("menu-expanded")
+}
+
+ScrollReveal({
+    origin: 'top',
+    distance: '3rem',
+    duration: 700,
+}).reveal(`
+    #home, 
+    #home img, 
+    #home .stats, 
+    #services,
+    #services header,
+    #services .card,
+    #about,
+    #about header,
+    #about .content`);
